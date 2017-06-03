df94778aef58fe71489173baf4b317ed9355ebfc
Clyde <chunz@allstate.com>
2017-06-03 20:25:28 +0100

    Completed basic UI tests for login page - no authentication implemented as yet

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,13 +1,23 @@
 function validateLoginAndSubmit() {
     var emailRegex = '.+\@.+\..+';
+    var passwordRegex = '^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{7,})';
 
     $('#loginEmailErrorMessage').removeClass('ErrorDisplay')
     $('#loginEmailErrorMessage').addClass('ErrorHidden')
+
+    $('#loginPasswordErrorMessage').removeClass('ErrorDisplay')
+    $('#loginPasswordErrorMessage').addClass('ErrorHidden')
 
     if (!$("#userIdEmail1").val().match(emailRegex)) {
         $('#loginEmailErrorMessage').html('Please provide a valid email address')
         $('#loginEmailErrorMessage').removeClass('ErrorHidden')
         $('#loginEmailErrorMessage').addClass('ErrorDisplay')
+    }
+
+    if (!$("#passwordField1").val().match(passwordRegex)) {
+        $('#loginPasswordErrorMessage').html('Please provide a password. Must be greater than 6 chars long and contain a mix of upper and lower case chars, and digits')
+        $('#loginPasswordErrorMessage').removeClass('ErrorHidden')
+        $('#loginPasswordErrorMessage').addClass('ErrorDisplay')
     }
 }
 
--- test/acceptance/login_test.js
+++ test/acceptance/login_test.js
@@ -15,14 +15,14 @@ AfterSuite((I) => {
 
 Scenario('LOGIN:TEST1: Validate login screen is available at root folder', (I) => {
     I.amOnPage('/');
-    I.see('Danny Boy Event Manager');
+    I.see('Danny Boy Jazz & Blues Event Manager');
     I.see('Username (email address)');
     I.see('Password');
 });
 
 Scenario('LOGIN:TEST2: Confirm an appropriate error message is displayed if invalid email address is not entered', (I) => {
     I.amOnPage('/');
-    I.see('Danny Boy Event Manager');
+    I.see('Danny Boy Jazz & Blues Event Manager');
     I.see('Username (email address)');
     I.see('Password');
 
@@ -34,7 +34,7 @@ Scenario('LOGIN:TEST2: Confirm an appropriate error message is displayed if inva
 
 Scenario('LOGIN:TEST3: Confirm an error message is not displayed if valid email address is not entered', (I) => {
     I.amOnPage('/');
-    I.see('Danny Boy Event Manager');
+    I.see('Danny Boy Jazz & Blues Event Manager');
     I.see('Username (email address)');
     I.see('Password');
 
@@ -46,7 +46,7 @@ Scenario('LOGIN:TEST3: Confirm an error message is not displayed if valid email 
 
 Scenario('LOGIN:TEST4: Confirm an appropriate error message is displayed if invalid email address is not entered, then disappears if corrected', (I) => {
     I.amOnPage('/');
-    I.see('Danny Boy Event Manager');
+    I.see('Danny Boy Jazz & Blues Event Manager');
     I.see('Username (email address)');
     I.see('Password');
 
@@ -59,4 +59,103 @@ Scenario('LOGIN:TEST4: Confirm an appropriate error message is displayed if inva
     I.click('#loginBtn')
 
     I.dontSee("Please provide a valid email address")
+});
+
+Scenario('LOGIN:TEST5: Confirm an appropriate error message is displayed if an email address is not entered', (I) => {
+    I.amOnPage('/');
+    I.see('Danny Boy Jazz & Blues Event Manager');
+    I.see('Username (email address)');
+    I.see('Password');
+
+    I.click('#loginBtn')
+
+    I.see("Please provide a valid email address")
+});
+
+Scenario('LOGIN:TEST6: Confirm an appropriate error message is displayed if a password is not entered', (I) => {
+    I.amOnPage('/');
+    I.see('Danny Boy Jazz & Blues Event Manager');
+    I.see('Username (email address)');
+    I.see('Password');
+
+    I.click('#loginBtn')
+
+    I.see("Please provide a password. Must be greater than 6 chars long and contain a mix of upper and lower case chars, and digits")
+});
+
+Scenario('LOGIN:TEST7: Confirm an error message is not displayed if a valid password is entered', (I) => {
+    I.amOnPage('/');
+    I.see('Danny Boy Jazz & Blues Event Manager');
+    I.see('Username (email address)');
+    I.see('Password');
+
+    I.fillField('#passwordField1', 'AValidpassw0rd')
+    I.click('#loginBtn')
+
+    I.dontSee("Please provide a password. Must be greater than 6 chars long and contain a mix of upper and lower case chars, and digits")
+});
+
+Scenario('LOGIN:TEST8: Confirm an error message is displayed if a password with no uppercase chars is entered', (I) => {
+    I.amOnPage('/');
+    I.see('Danny Boy Jazz & Blues Event Manager');
+    I.see('Username (email address)');
+    I.see('Password');
+
+    I.fillField('#passwordField1', 'avalidpassw0rd')
+    I.click('#loginBtn')
+
+    I.see("Please provide a password. Must be greater than 6 chars long and contain a mix of upper and lower case chars, and digits")
+});
+
+Scenario('LOGIN:TEST9: Confirm an error message is displayed if a password with no lowercase chars is entered', (I) => {
+    I.amOnPage('/');
+    I.see('Danny Boy Jazz & Blues Event Manager');
+    I.see('Username (email address)');
+    I.see('Password');
+
+    I.fillField('#passwordField1', 'avalidpassw0rd')
+    I.click('#loginBtn')
+
+    I.see("Please provide a password. Must be greater than 6 chars long and contain a mix of upper and lower case chars, and digits")
+});
+
+Scenario('LOGIN:TEST10: Confirm an error message is displayed if a password with no digits is entered', (I) => {
+    I.amOnPage('/');
+    I.see('Danny Boy Jazz & Blues Event Manager');
+    I.see('Username (email address)');
+    I.see('Password');
+
+    I.fillField('#passwordField1', 'avalidpassw0rd')
+    I.click('#loginBtn')
+
+    I.see("Please provide a password. Must be greater than 6 chars long and contain a mix of upper and lower case chars, and digits")
+});
+
+Scenario('LOGIN:TEST11: Confirm an error message is displayed if an invalid password is entered, then disappers when a valid is entered', (I) => {
+    I.amOnPage('/');
+    I.see('Danny Boy Jazz & Blues Event Manager');
+    I.see('Username (email address)');
+    I.see('Password');
+
+    I.fillField('#passwordField1', 'avalidpassw0rd')
+    I.click('#loginBtn')
+
+    I.see("Please provide a password. Must be greater than 6 chars long and contain a mix of upper and lower case chars, and digits")
+
+    I.fillField('#passwordField1', 'aValidpassw0rd')
+    I.click('#loginBtn')
+
+    I.dontSee("Please provide a password. Must be greater than 6 chars long and contain a mix of upper and lower case chars, and digits")
+});
+
+Scenario('LOGIN:TEST12: Confirm both error messages are displayed no values are entered', (I) => {
+    I.amOnPage('/');
+    I.see('Danny Boy Jazz & Blues Event Manager');
+    I.see('Username (email address)');
+    I.see('Password');
+
+    I.click('#loginBtn')
+
+    I.see("Please provide a valid email address")
+    I.see("Please provide a password. Must be greater than 6 chars long and contain a mix of upper and lower case chars, and digits")
 });
