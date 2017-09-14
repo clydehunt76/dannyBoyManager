6b4ce5fe0d5596fbf766f086571368910e7f116e
Clyde Hunt and Shamus Harkin <pair+chunz+shamu.harkin@allstate.com>
2017-09-14 10:54:01 +0100

    Still working on mocking passport

--- test/mocks/loginHelper.js
+++ test/mocks/loginHelper.js
@@ -3,16 +3,37 @@
 module.exports = {
   initialize: function() {
     console.log("Initialized");;
+    return function(req, res, next) {
+       passport = this;
+       passport._key = 'passport';
+       passport._userProperty = 'user';
+       passport.serializeUser = function(user, done) {
+           return done(null, user.id);
+       };
+       passport.deserializeUser = function(user, done) {
+           return done(null, user);
+       };
+       req._passport = {
+           instance: passport
+       };
+       req._passport.session = {
+           user: new app.models.User({ id: 1, name: 'A Person' })
+       };
+
+       return next();
+    }
   },
   session: function() {
     console.log("Sessioned");
+    return function(req, res, next) { return next() }
   },
   use: function() {
     console.log("Used");
   },
   authenticate: function() {
     console.log("Authenticated")
-  }(req, res, next),
+    return function(req, res, next) { return next() }
+  },
   log: function() {
     console.log("Successfully Mocked");
   }
